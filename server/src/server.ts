e620913406581ea6c1bc4aaa9e63dd500cdc9f76
Shriyash <ghimires1@my.smccd.edu>
2026-07-22 23:50:47 -0700

    feat: api init

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,1 +1,8 @@
 import express from "express";
+import config from "./config/config";
+import { type Response, type Request } from "express";
+const app = express();
+
+app.listen(config.port, () => {
+  console.log(`Live: http://localhost:${config.port}`);
+});
