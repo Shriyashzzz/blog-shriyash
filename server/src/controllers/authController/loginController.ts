6ee0f081f0a00e27d3b82892ef0ad1709472b49f
Shriyash <ghimires1@my.smccd.edu>
2026-07-27 13:08:26 -0700

    feat: validate incoming auth payload

--- server/src/controllers/authController/loginController.ts
+++ server/src/controllers/authController/loginController.ts
@@ -4,55 +4,70 @@ import bcrypt from "bcryptjs";
 import { setTokenCookie } from "../../ultility/cookie";
 import config from "../../config/config";
 import { Role } from "../../../generated/prisma/enums";
+import { validationResult, matchedData, body } from "express-validator";
 
-const loginController = async (
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) => {
-  //Incomplete!: validate the incoming payload from the clien // correct errors on empty form | correct error on non existing users
-  const { email, password } = req.body;
+const loginValidator = [
+  body("email")
+    .trim()
+    .notEmpty()
+    .withMessage("Email cannot be empty")
+    .isEmail()
+    .withMessage("Please enter a vaid email address")
+    .normalizeEmail(),
+  body("password").trim().notEmpty().withMessage("Password field is Empty"),
+];
 
-  if (!email || !password)
-    return res
-      .status(400)
-      .json({ message: "Either Email or Password field is empty" });
-  const user = await prisma.user.findUnique({ where: { email: email } });
-  if (!user)
-    return res.status(400).json({ message: "This account does not exist" });
-  const isValidPass = await bcrypt.compare(password, user.password);
-  if (!isValidPass)
-    return res.status(401).json({ message: "Incorrect Password" });
-  try {
-    const cookieOptions = {
-      // branch this out to make this function completely independent
-      httpOnly: true,
-      secure: config.nodeEnv === "DEV" ? false : true,
-      sameSite: "lax",
-      maxAge: user.role === Role.Member ? 604800000 : 172800000, // 7days if an member, 2 days for admins both in miliseconds // remember to change token "expieresIn" too if you change this!
-      path: "/",
-    };
-    const tokenPayload = {
-      username: user.username,
-      id: user.id,
-      role: user.role,
-    };
-    const tokenOptions = {
-      expiresIn: user.role === Role.Member ? "7d" : "2d", //remember to change cookies "maxAge" too if you change this!
-    };
-    const response = await setTokenCookie(
-      res,
-      "auth_token",
-      cookieOptions,
-      tokenPayload,
-      tokenOptions,
-    );
-    if (response) return res.status(200).json({ message: "User logged in!" });
-    return res.sendStatus(501);
-  } catch (e) {
-    console.error(e);
-    return res.sendStatus(501);
-  }
-};
+const loginController = [
+  ...loginValidator,
+  async (req: Request, res: Response, next: NextFunction) => {
+    //Incomplete!: validate the incoming payload from the client // correct errors on empty form | correct error on non existing users
+    const errors = validationResult(req);
+    if (!errors.isEmpty())
+      return res
+        .status(400)
+        .json({ message: "Invalid Login Info", errors: errors.array() });
+    const { email, password } = matchedData(req);
+    if (!email || !password)
+      return res
+        .status(400)
+        .json({ message: "Either Email or Password field is empty" });
+    const user = await prisma.user.findUnique({ where: { email: email } });
+    if (!user)
+      return res.status(400).json({ message: "This account does not exist" });
+    const isValidPass = await bcrypt.compare(password, user.password);
+    if (!isValidPass)
+      return res.status(401).json({ message: "Incorrect Password" });
+    try {
+      const cookieOptions = {
+        // branch this out to make this function completely independent
+        httpOnly: true,
+        secure: config.nodeEnv === "DEV" ? false : true,
+        sameSite: "lax",
+        maxAge: user.role === Role.Member ? 604800000 : 172800000, // 7days if an member, 2 days for admins both in miliseconds // remember to change token "expieresIn" too if you change this!
+        path: "/",
+      };
+      const tokenPayload = {
+        username: user.username,
+        id: user.id,
+        role: user.role,
+      };
+      const tokenOptions = {
+        expiresIn: user.role === Role.Member ? "7d" : "2d", //remember to change cookies "maxAge" too if you change this!
+      };
+      const response = await setTokenCookie(
+        res,
+        "auth_token",
+        cookieOptions,
+        tokenPayload,
+        tokenOptions,
+      );
+      if (response) return res.status(200).json({ message: "User logged in!" });
+      return res.sendStatus(501);
+    } catch (e) {
+      console.error(e);
+      return res.sendStatus(501);
+    }
+  },
+];
 
 export default loginController;
--- server/src/controllers/authController/signUpController.ts
+++ server/src/controllers/authController/signUpController.ts
@@ -4,54 +4,96 @@ import bcrypt from "bcryptjs";
 import { setTokenCookie } from "../../ultility/cookie";
 import config from "../../config/config";
 import { Role } from "../../../generated/prisma/enums";
+import { validationResult, matchedData, body } from "express-validator";
 
-const signUpController = async (
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) => {
-  // Incomplete: Make sure to validate the incoming payload including cheking if user already exists in a db!
-  const { username, email, password, cpassword } = req.body;
-  //hash password, call authqueries method to create new user, if unsuccessfull handle the error gracefully, will have an error message with the response
-  const hashedPassword = await bcrypt.hash(password, 12);
-  const response = await authQueries.signUpNewUser(
-    email,
-    hashedPassword,
-    username,
-  );
-  if (!response.success)
-    return res.status(500).json({ message: "Server Error" });
-  if (response.user) {
-    // ------**cookie options**----------
-    const cookieOptions = {
-      httpOnly: true,
-      secure: config.nodeEnv === "DEV" ? false : true,
-      sameSite: "lax",
-      maxAge: response.user.role === Role.Member ? 604800000 : 172800000, // 7days if an member, 2 days for admins both in miliseconds // remember to change token "expieresIn" too if you change this!
-      path: "/",
-    };
-    const tokenPayload = {
-      username: response.user.username,
-      id: response.user.id,
-      role: response.user.role,
-    };
-    const tokenOptions = {
-      expiresIn: response.user.role === Role.Member ? "7d" : "2d", //remember to change cookies "maxAge" too if you change this!
-    };
-    // ------------**cookie options**-----------
-    const isCookieAdded = await setTokenCookie(
-      res,
-      "auth_token",
-      cookieOptions,
-      tokenPayload,
-      tokenOptions,
-    );
-    if (isCookieAdded)
+const signUpValidator = [
+  body("username").trim().notEmpty().withMessage("Username cannot be empty"),
+  body("email")
+    .trim()
+    .notEmpty()
+    .withMessage("Email cannot be empty")
+    .isEmail()
+    .withMessage("Please enter a vaid email address")
+    .normalizeEmail(),
+  body("password")
+    .trim()
+    .notEmpty()
+    .withMessage("Password is required")
+    .isLength({ min: 8 })
+    .withMessage("Password must be at least 8 characters long")
+    .matches(/[A-Z]/)
+    .withMessage("Password must contain at least one uppercase letter")
+    .matches(/[a-z]/)
+    .withMessage("Password must contain at least one lowercase letter")
+    .matches(/[0-9]/)
+    .withMessage("Password must contain at least one number")
+    .matches(/[#$@!%&*?]/)
+    .withMessage("Password must contain at least one special character"),
+  body("cpassword")
+    .trim()
+    .custom((value, { req }) => {
+      if (value === req.body.password) {
+        return true;
+      }
+      throw new Error("Passwords Do not Match");
+    }),
+];
+
+const signUpController = [
+  ...signUpValidator,
+  async (req: Request, res: Response, next: NextFunction) => {
+    const errors = validationResult(req);
+
+    // ------------------------**validating incoming user payload **---------------------------
+    if (!errors.isEmpty())
       return res
-        .status(200)
-        .json({ message: "user id now logged in", redirectUrl: "/" });
-  } else {
-    return res.status(501).json({ message: "Error saving the user info " });
-  }
-};
+        .status(400)
+        .json({ message: "Invlaid Sign Up payload", errors: errors.array() });
+    const { username, email, password, cpassword } = matchedData(req);
+    // -------------------------------------**********--------------------------------------
+    const hashedPassword = await bcrypt.hash(password, 12);
+    const response = await authQueries.signUpNewUser(
+      email,
+      hashedPassword,
+      username,
+    );
+    if (!response.success)
+      return res.status(500).json({ message: "Server Error" });
+    if (response.user) {
+      // ------**cookie options**----------
+      const cookieOptions = {
+        httpOnly: true,
+        secure: config.nodeEnv === "DEV" ? false : true,
+        sameSite: "lax",
+        maxAge: response.user.role === Role.Member ? 604800000 : 172800000, // 7days if an member, 2 days for admins both in miliseconds // remember to change token "expieresIn" too if you change this!
+        path: "/",
+      };
+      const tokenPayload = {
+        username: response.user.username,
+        id: response.user.id,
+        role: response.user.role,
+      };
+      const tokenOptions = {
+        expiresIn: response.user.role === Role.Member ? "7d" : "2d", //remember to change cookies "maxAge" too if you change this!
+      };
+      // ------------**************************-----------
+      const isCookieAdded = await setTokenCookie(
+        res,
+        "auth_token",
+        cookieOptions,
+        tokenPayload,
+        tokenOptions,
+      );
+      if (isCookieAdded) {
+        return res
+          .status(200)
+          .json({ message: "user id now logged in", redirectUrl: "/" });
+      } else {
+        return res.status(500).json({ message: "Could not set auth cookie" });
+      }
+    } else {
+      return res.status(501).json({ message: "Error saving the user info " });
+    }
+  },
+];
 export default signUpController;
