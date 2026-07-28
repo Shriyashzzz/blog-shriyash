7d16874b68c2fcc67a0a7566624d22e2d7bbcc60
Shriyash <ghimires1@my.smccd.edu>
2026-07-27 23:59:37 -0700

    refactor: validated incoming payload on newComment endpoint

--- server/src/controllers/commentController.ts
+++ server/src/controllers/commentController.ts
@@ -1,33 +1,40 @@
 import { type Request, type Response, type NextFunction } from "express";
 import queries from "../models/queries";
 import _ from "lodash";
+import { validationResult, matchedData, body, param } from "express-validator";
 
-const newCommentController = async (
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) => {
-  const { postId } = req.params;
-  const intPostId = _.toInteger(postId);
-  // validate the incoming payload later
-  const { commentContent } = req.body;
-  if (!req.user) return res.status(401).json("Error: User not logged in!");
-  const response = await queries.addnewComment(
-    intPostId,
-    commentContent,
-    req.user.id,
-  );
-  if (response.ok) {
-    const allComments = (await queries.getPostComments(intPostId)).comments;
-    res.status(200).json({
-      message: "Comment Added",
-      comments: allComments,
-    });
-  } else {
-    res.status(500).json({
-      message: "Server Error adding new Comment",
-    });
-  }
-};
+const newCommentValidator = [
+  body("commentContent")
+    .trim()
+    .notEmpty()
+    .withMessage("Comment Cannot be empty!"),
+  param("postId").notEmpty().withMessage("PostId parameter not found"),
+];
+const newCommentController = [
+  ...newCommentValidator,
+  async (req: Request, res: Response, next: NextFunction) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) res.status(400).json({ errors: errors.array() });
+    const { commentContent, postId } = matchedData(req);
+    const intPostId = _.toInteger(postId);
+    if (!req.user) return res.status(401).json("Error: User not logged in!");
+    const response = await queries.addnewComment(
+      intPostId,
+      commentContent,
+      req.user.id,
+    );
+    if (response.ok) {
+      const allComments = (await queries.getPostComments(intPostId)).comments;
+      res.status(200).json({
+        message: "Comment Added",
+        comments: allComments,
+      });
+    } else {
+      res.status(500).json({
+        message: "Server Error adding new Comment",
+      });
+    }
+  },
+];
 
 export { newCommentController };
