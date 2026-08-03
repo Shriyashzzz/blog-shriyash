3ab601857854683037c2d23f68f17be85592bc4f
Shriyash <ghimires1@my.smccd.edu>
2026-08-03 16:43:27 -0700

    fix: make header para more repsonsive

--- client-User/src/components/Header.tsx
+++ client-User/src/components/Header.tsx
@@ -9,10 +9,10 @@ export function Header() {
   const theme = useSelector((state: RootState) => state.theme.value);
   const dispatch = useDispatch();
   return (
-    <header className="flex w-full flex-row items-center justify-between border-b-2 border-green-800 px-6">
-      <div className="flex flex-row items-center gap-2 p-2">
+    <header className="flex w-full flex-row items-center justify-between border-b-2 border-green-800 px-6 py-2">
+      <div className="flex flex-row items-center gap-2">
         <img src={terminal} alt="blog logo" />
-        <p className="font-[Nabla] text-2xl text-green-600">
+        <p className="m-0 p-0 font-[Nabla] text-sm text-green-600 sm:text-xl">
           &lt;Shriyash Uncompiled / &gt;
         </p>
       </div>
