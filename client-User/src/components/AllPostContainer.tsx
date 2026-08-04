e7ee9b9bd3b52777914f611cd402376117466e39
Shriyash <ghimires1@my.smccd.edu>
2026-08-03 22:13:49 -0700

    feat: fetch form api and display published posts

--- client-User/src/components/AllPostContainer.tsx
+++ client-User/src/components/AllPostContainer.tsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
 import useFetch from "../hooks/useFetch";
 import { useEffect } from "react";
+import Markdown from "react-markdown";
+import remarkGm from "remark-gfm";
 
 interface Post {
   authorId: number;
   content: string;
-  createdAt: Date;
+  createdAt: string;
   id: number;
   title: string;
   viewCount: number;
@@ -32,5 +34,20 @@ export function AllPostContainer() {
     return <>Loading</>;
   }
 
-  return <section></section>;
+  return (
+    <section className="m-5 grid auto-rows-fr grid-cols-1 gap-4">
+      {posts.map((post) => {
+        return (
+          <article className="flex h-full flex-col bg-gray-100 p-2 dark:bg-gray-500">
+            <div>
+              <h1 className="font-bold">{post.title}</h1>
+            </div>
+            <p className="line-clamp-3 truncate">
+              <Markdown>{post.content}</Markdown>
+            </p>
+          </article>
+        );
+      })}
+    </section>
+  );
 }
