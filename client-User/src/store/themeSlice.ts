d4599e2539dbd2ce4ddd469df7234affec64fa7a
Shriyash <ghimires1@my.smccd.edu>
2026-08-04 23:45:01 -0700

    feat: save user theme preference on localstorage

--- client-User/src/store/themeSlice.ts
+++ client-User/src/store/themeSlice.ts
@@ -1,13 +1,31 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+const getDeviceTheme = (): "light" | "dark" => {
+  const localTheme = localStorage.getItem("blog_shriyash_theme");
+
+  if (localTheme === "light" || localTheme === "dark") return localTheme;
+  const isDarkMode: boolean = window.matchMedia(
+    "(prefers-color-scheme: dark)",
+  ).matches;
+
+  if (isDarkMode) {
+    localStorage.setItem("blog_shriyash_theme", "dark");
+    return "dark";
+  }
+  localStorage.setItem("blog_shriyash_theme", "light");
+  return "light";
+};
+
 export const themeSlice = createSlice({
   name: "theme",
-  initialState: { value: "light" },
+  initialState: { value: getDeviceTheme() },
   reducers: {
     switchTheme: (state) => {
       if (state.value == "light") {
+        localStorage.setItem("blog_shriyash_theme", "dark");
         state.value = "dark";
       } else {
+        localStorage.setItem("blog_shriyash_theme", "light");
         state.value = "light";
       }
     },
