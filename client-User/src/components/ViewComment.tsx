980e2bdaccb45eea8fcbb0e54f01047456383186
Shriyash <ghimires1@my.smccd.edu>
2026-08-05 14:26:28 -0700

    feat: change author pfp and style

--- client-User/src/components/ViewComment.tsx
+++ client-User/src/components/ViewComment.tsx
@@ -51,10 +51,12 @@ export function ViewComments({ comments, setComments }: CommentProp) {
             className="flex flex-row gap-4 bg-gray-100 p-2 dark:bg-gray-800"
           >
             <Avatar
+              size="3"
+              radius="full"
               className="mt-2 border border-gray-300"
               src={
                 cmt.author.role == "Admin"
-                  ? "https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExbjRqbHBiaTVqemJtZHFlcjlldjMycm9xNjV0NmUyeGlzYmFrYjVjYSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/atQF1zaSGq8s8/giphy.gif"
+                  ? "https://scontent-sjc6-1.xx.fbcdn.net/v/t39.30808-6/684262166_26612009511790748_2315032028178435336_n.jpg?stp=cp6_dst-jpg_tt6&cstp=mx2048x2048&ctp=s2048x2048&_nc_cat=100&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=r-iOFVaVBREQ7kNvwFSiHTI&_nc_oc=AdpxCQ2Llo91ug6hX1E0jvPx8aewLeH0Tlu3LnDlkU9xTrydym602IztwBsgqyalqvJ8onM8GYXlHsFyhm6jXfE6&_nc_zt=23&_nc_ht=scontent-sjc6-1.xx&_nc_gid=_tntilGb5NTS23LVOdpo8g&_nc_ss=7b2a8&oh=00_AQG1oDZtmr36czaM4cRjECG2Ezr0wgNiTBQJ8W8uSMkSqw&oe=6A78F759"
                   : undefined
               }
               fallback={initialExtract(cmt.author.username)}
