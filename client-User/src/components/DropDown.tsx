32ec58bf3db8540b5543c39b6181b8ee9666d5d4
Shriyash <ghimires1@my.smccd.edu>
2026-08-05 20:04:43 -0700

    feat: add user profile drop down avatar

--- client-User/src/components/DropDown.tsx
+++ client-User/src/components/DropDown.tsx
@@ -3,11 +3,15 @@ import { HamburgerMenuIcon } from "@radix-ui/react-icons";
 import { useDispatch } from "react-redux";
 import { isNotAuth } from "../store/authSlice";
 import { useNavigate } from "react-router";
+import { useSelector } from "react-redux";
+import { RootState } from "../store/store";
+import { initialExtract } from "../utils/initialExtractor";
+import { Avatar } from "@radix-ui/themes";
 
 export const DropDown = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
-
+  const user = useSelector((state: RootState) => state.auth.value.user);
   const handleLogout = async () => {
     try {
       const response = await fetch("/api/auth/logout", {
@@ -35,10 +39,20 @@ export const DropDown = () => {
     <DropdownMenu.Root modal={false}>
       <DropdownMenu.Trigger asChild>
         <button
-          className="inline-flex size-8.75 cursor-pointer items-center justify-center rounded-full bg-white text-gray-700 shadow-[0_2px_10px] shadow-black/20 outline-none hover:bg-gray-100 focus:shadow-[0_0_0_2px] focus:shadow-black"
+          className="inline-flex size-8.75 cursor-pointer items-center justify-center rounded-full text-black shadow-[0_2px_10px] shadow-black/20 outline-none hover:bg-gray-100 focus:shadow-[0_0_0_2px] focus:shadow-black dark:hover:bg-gray-700"
           aria-label="Options"
         >
-          <HamburgerMenuIcon />
+          <Avatar
+            size="3"
+            radius="full"
+            className="border *:border-gray-300"
+            src={
+              user!.role == "Admin"
+                ? "https://scontent-sjc6-1.xx.fbcdn.net/v/t39.30808-6/684262166_26612009511790748_2315032028178435336_n.jpg?stp=cp6_dst-jpg_tt6&cstp=mx2048x2048&ctp=s2048x2048&_nc_cat=100&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=r-iOFVaVBREQ7kNvwFSiHTI&_nc_oc=AdpxCQ2Llo91ug6hX1E0jvPx8aewLeH0Tlu3LnDlkU9xTrydym602IztwBsgqyalqvJ8onM8GYXlHsFyhm6jXfE6&_nc_zt=23&_nc_ht=scontent-sjc6-1.xx&_nc_gid=_tntilGb5NTS23LVOdpo8g&_nc_ss=7b2a8&oh=00_AQG1oDZtmr36czaM4cRjECG2Ezr0wgNiTBQJ8W8uSMkSqw&oe=6A78F759"
+                : undefined
+            }
+            fallback={initialExtract(user!.username)}
+          />
         </button>
       </DropdownMenu.Trigger>
 
