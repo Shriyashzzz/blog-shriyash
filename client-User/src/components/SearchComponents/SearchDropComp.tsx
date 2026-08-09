ebd1ba2367bdce83b90cba5f46999cdee8479333
Shriyash <ghimires1@my.smccd.edu>
2026-08-08 23:12:26 -0700

    feat: navigate to the post on click on search result component

--- client-User/src/components/SearchComponents/SearchDropComp.tsx
+++ client-User/src/components/SearchComponents/SearchDropComp.tsx
@@ -1,5 +1,5 @@
-import { CalendarIcon } from "@radix-ui/react-icons";
 import { iso8061DateParser } from "../../utils/ISO8061DateParser";
+import { useNavigate } from "react-router";
 
 interface SearchCompParams {
   post: {
@@ -14,8 +14,18 @@ interface SearchCompParams {
 
 export function SearchDropComp({ post }: SearchCompParams) {
   const stringDateIso = post.createdAt.toString();
+  const navigate = useNavigate();
+
+  const viewBlog = (postId: number) => {
+    if (!postId) return;
+    navigate(`/viewpost/${postId}`, { viewTransition: true });
+  };
+
   return (
-    <div className="group dark: w-full cursor-pointer border-b bg-gray-200 p-2 dark:bg-gray-500">
+    <div
+      onClick={() => viewBlog(post.id)}
+      className="group dark: w-full cursor-pointer border-b bg-gray-200 p-2 dark:bg-gray-500"
+    >
       <p className="text-sm">{`@${post.author.username.toLowerCase()}`}</p>
       <h1 className="font-bold group-hover:text-blue-800">{post.title}</h1>
       <p className="text-sm">{iso8061DateParser(stringDateIso)}</p>
